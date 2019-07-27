83b4c0c8e9f6013db396906a5ee6d7be9ed383c9
SyberHexen <SyberHexen@gmail.com>
2019-07-27 10:38:04 -0700

    payton: update workaround for vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,8 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
 # Workaround for error copying vendor files to recovery ramdisk
-TARGET_COPY_OUT_VENDOR := system/vendor
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 
 # Installer
 USE_RECOVERY_INSTALLER := true
